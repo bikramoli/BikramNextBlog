6180fefc9d3f335663e478c0af1b02fabbfd6804
bikramoli <bikramoli75@gmail.com>
2021-12-14 20:43:16 +0545

    Navbar with along with its items

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,14 +1,80 @@
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import { BiTerminal } from "react-icons/bi";
 import { SiAboutDotMe } from "react-icons/si";
-import { BiMoon } from "react-icons/bi";
+import { BiSun, BiMoon } from "react-icons/bi";
 import { VscGithub } from "react-icons/vsc";
 import { AiOutlineGoogle } from "react-icons/ai";
+import { auth, provider } from "../Firebase/Firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { IoLogOutOutline } from "react-icons/io5";
+import Alert from "./Alert";
 
+function Navbar() {
+  const [isMounted, setIsMounted] = useState(false);
+  const [isLogin, setLogin] = useState(false);
+  const { theme, setTheme } = useTheme();
+  const [viewAlert, setViewAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
-function Navbar(){
-  return(
+  useEffect(() => {
+    setIsMounted(true);
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      setLogin(true);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (isMounted) {  
+      setTheme(theme === "light" ? "dark" : "light");
+    }
+  };
+  const handelSignOut = () => {
+    signOut(auth)
+      .then((res) => {
+        setLogin(false);
+        localStorage.removeItem("user");
+
+        setViewAlert(true);
+        setAlertMessage("Hope to see you again !!");
+        setTimeout(() => {
+          setViewAlert(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handelSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({
+            name: res.user.displayName,
+            photo: res.user.photoURL,
+            token: res.user.accessToken,
+            uid: res.user.uid,
+          })
+        );
+        setLogin(true);
+        setViewAlert(true);
+        setAlertMessage(`Hello ${res.user.displayName}`);
+        setTimeout(() => {
+          setViewAlert(false);
+        }, 2000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
     <>
+      <Alert show={viewAlert} type="success" message={alertMessage} />
       <header className="fixed w-full border-t-4 bg-white dark:bg-dark border-indigo-600 dark:border-indigo-900 shadow dark:shadow-2 z-50">
         <div className="container mx-auto px-6 py-5">
           <div className="flex items-center justify-between">
@@ -19,7 +85,7 @@ function Navbar(){
                     <BiTerminal className="text-xl" />
                   </span>
                   <span className="mx-3 font-semibold text-base md:text-base">
-                    Web-infosys 1.0
+                    Bits-0f-C0de
                   </span>
                 </a>
               </Link>
@@ -28,16 +94,20 @@ function Navbar(){
             <div className="flex items-center -mx-2">
               <button
                 className="flex items-center mx-2 lg:mx-4 text-base text-gray-800 hover:text-indigo-600 dark:text-gray-50"
-              
+                onClick={toggleTheme}
               >
                 <span className="text-lg">
-                <BiMoon className="text-xl" />
+                  {isMounted && theme === "dark" ? (
+                    <BiSun className="text-xl" />
+                  ) : (
+                    <BiMoon className="text-xl" />
+                  )}
                 </span>
               </button>
 
               <a
                 className="flex items-center mx-2 lg:mx-4 text-gray-800 hover:text-indigo-600 dark:text-gray-50"
-                href="https://github.com/"
+                href="https://github.com/soumyajit4419"
                 rel="noopener noreferrer"
                 target="_blank"
               >
@@ -48,14 +118,24 @@ function Navbar(){
 
               <button className="flex items-center mx-2 lg:mx-4 text-base text-gray-800 hover:text-indigo-600 dark:text-gray-50">
                 <span className="text-lg">
-                 <AiOutlineGoogle/>
+                  {isLogin ? (
+                    <IoLogOutOutline
+                      className="text-xl"
+                      onClick={handelSignOut}
+                    />
+                  ) : (
+                    <AiOutlineGoogle
+                      className="text-xl"
+                      onClick={handelSignIn}
+                    />
+                  )}
                 </span>
               </button>
 
               <Link href="/about">
                 <a className="flex items-center mx-2  lg:mx-4 text-base text-gray-800 hover:text-indigo-600 dark:text-gray-50">
                   <span className="text-xl ">
-                   H
+                    <SiAboutDotMe className="text-xl" />
                   </span>
                 </a>
               </Link>
@@ -64,6 +144,7 @@ function Navbar(){
         </div>
       </header>
     </>
-  )
+  );
 }
+
 export default Navbar;
