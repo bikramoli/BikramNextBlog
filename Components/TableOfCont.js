851471be79fd09486412749154a1f26df8c49cf7
bikramoli <bikramoli75@gmail.com>
2021-12-16 11:59:36 +0545

    changing color of active heading when click

--- Components/TableOfCont.js
+++ Components/TableOfCont.js
@@ -1,15 +1,20 @@
 import Link from "next/link";
-import React from "react";
+import React,{useState} from "react";
 
 
 function TableOfContent({headings}) {
+    const [active, setActive] = useState("")
     return(
         <nav className="sticky top-32 overflow-auto toc-inner">
             <ul>
             {headings.map((heading, index)=>
                 <li key={heading.id}  
                 className="mt-4 text-lg text-gray-700 dark:text-gray-400"
-                style={{paddingLeft:"1rem", color: "#6366f1"}}
+                style={{paddingLeft:"1rem", 
+                color: heading.id===active? "#6366f1": ""}}
+                onClick={()=>{
+                    setActive(heading.id)
+                }}
                 >
                   <Link href={`#${heading.id}`}>{heading.text}</Link>  
                 </li>
