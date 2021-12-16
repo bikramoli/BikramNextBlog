277a77d46f295afdc2d5a5975dcf5008e5a1f153
bikramoli <bikramoli75@gmail.com>
2021-12-16 11:44:59 +0545

    adding style to table of content

--- Components/TableOfCont.js
+++ Components/TableOfCont.js
@@ -8,7 +8,9 @@ function TableOfContent({headings}) {
             <ul>
             {headings.map((heading, index)=>
                 <li key={heading.id}  
-                className="mt-4 text-lg text-gray-700 dark:text-gray-400">
+                className="mt-4 text-lg text-gray-700 dark:text-gray-400"
+                style={{paddingLeft:"1rem", color: "#6366f1"}}
+                >
                   <Link href={`#${heading.id}`}>{heading.text}</Link>  
                 </li>
             )}
