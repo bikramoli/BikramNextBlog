228364de87ba915d4a2c4b5ce5c5c1daa409fe70
bikramoli <bikramoli75@gmail.com>
2021-12-16 09:29:20 +0545

    Decoration for heading

--- Components/TableOfCont.js
+++ Components/TableOfCont.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import { Vlogs } from "../pages/Data";
+
 function TableOfContent() {
     return(
-        <h1>Table of content here..</h1>
+        <div>
+        {Vlogs.map((vlog, index)=>
+            <li key={index}  
+            className="mt-4 text-lg text-gray-700 dark:text-gray-400">
+                {vlog.title}
+            </li>
+        )}
+        </div>
     )
 }
 export default TableOfContent;
