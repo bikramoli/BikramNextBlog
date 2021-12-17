54ffbbf2dc1c8c685c3f59e518ea95964b818db5
bikramoli <bikramoli75@gmail.com>
2021-12-17 17:46:55 +0545

    popular content hover and effect

--- Components/PopularContent.js
+++ Components/PopularContent.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 
 
 function PopularContent({dat}) {
     return (
+        
         <div>
             <h2 className=' m-2 mb-5 mb-10 font-bold text-indigo-900'>POPULAR CONTENT</h2>  
-            {dat.map((d, i)=><li key={i}>{d.data.Title}</li>)}                 
+            {dat.map((d, i)=><Link href={`/blogs/${d.data.Title.split(' ').join('-').toLowerCase()}`}><li key={i} 
+            className="m-2 hover:text-indigo-900 underline list-none cursor-pointer">-{d.data.Title}</li></Link>)}                 
         </div>
     )
 }
