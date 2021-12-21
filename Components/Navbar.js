b871b13320d96a5417a88232cfc8094ac1e8132b
bikramoli <bikramoli75@gmail.com>
2021-12-21 23:43:46 +0545

    check if online or offline, show icon based on it

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -2,11 +2,10 @@ import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import { BiCode, BiToggleLeft, BiToggleRight, BiWifi, BiWifiOff} from "react-icons/bi";
-import { IoLogOutOutline } from "react-icons/io5";
-import { AiOutlineGoogle } from "react-icons/ai";
+import { AiFillDownSquare,  AiOutlineGoogle } from "react-icons/ai";
 import {signInWithPopup, signOut} from "firebase/auth";
 import { auth, provider } from "../Firebase/Firebase";
-
+import { Offline, Online } from "react-detect-offline";
 
 function Navbar({scrollHeight}) {
   const { theme, setTheme } = useTheme();
@@ -16,7 +15,7 @@ function Navbar({scrollHeight}) {
   const [photo, setPhoto] = useState("");
 
   useEffect(() => {
-  setStatus(window.ononline)
+  setStatus(!window.ononline)
   const user = JSON.parse(localStorage.getItem('user'));
    if (user){
      setIsLogin(true); 
@@ -58,7 +57,7 @@ function Navbar({scrollHeight}) {
     .catch((error)=>{
       console.log(error);
     })
-  }
+  };
   const toggleTheme = () => {
       setTheme(theme === "light" ? "dark" : "light");
   };
@@ -66,13 +65,13 @@ function Navbar({scrollHeight}) {
   return (
     <>
       <header className= "fixed w-full border-t-4 bg-white dark:bg-gray-900 border-indigo-600 dark:border-indigo-900 shadow dark:shadow-2 z-50" style={{borderBottom: scrollHeight >= 50 ? theme==="dark"?'solid indigo':'':''}}>
-        <div className="container mx-auto px-6 py-5">
+        <div className="container mx-auto px-2 py-2">
           <div className="flex items-center justify-between">
             <div>
               <Link href="/">
                 <a className="flex items-center hover:text-indigo-600 text-gray-800 dark:text-gray-50">
                   <span className="text-xl font-semibold">
-                    <BiCode className="text-xl" />
+                    <BiCode className="text-xl"/>
                   </span>
                   <span className="mx-3 font-bold text-base text-red-600 md:text-base">
                     Digit-Infosys
@@ -94,29 +93,42 @@ function Navbar({scrollHeight}) {
                   )}
                 </span>
               </button>
-
               <a
                 className="flex items-center mx-2 lg:mx-4 text-gray-800 hover:text-indigo-600 dark:text-gray-50"
                 href="https://github.com/"
                 rel="noopener noreferrer"
                 target="_blank"
               >
+                {/* show wifi if online else nowifi */}
                 <span className="text-lg">
-                  {status ?
-                  <BiWifi className="text-lg" />:
-                  <BiWifiOff className="text-lg" />
-                  }
-                  
+                    <Online><BiWifi className="text-lg" /></Online>
+                    <Offline><BiWifiOff className="text-lg" /></Offline>
                 </span>
               </a>
 
               <button className="flex items-center mx-2 lg:mx-4 text-base text-gray-800 hover:text-indigo-600 dark:text-gray-50">
               <span className="text-lg">
                   {isLogin ? (
-                    <IoLogOutOutline
-                      className="text-xl"
-                      onClick={handelSignOut}
-                    />
+                  <div className="flex-shrink-0 mr-1.5 md:mr-3 im">
+                    <div className="dropdown inline-block relative">
+                      <button className="bg-gray-100 text-gray-700 font-semibold py-1 px-2 rounded inline-flex items-center">
+                      <span className="mr-1">
+                        <img className="mt-0 rounded-full w-8 h-8 sm:w-8 sm:h-8"
+                          src={photo}
+                          alt={name}/>
+                      </span>
+                        <AiFillDownSquare/>
+                      </button>
+                      <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+                        <li><a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" >setting</a></li>
+                        <li>
+                          <a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" onClick={handelSignOut}>
+                            Logout
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                </div>
                   ) : (
                     <AiOutlineGoogle
                       className="text-xl"
@@ -125,18 +137,6 @@ function Navbar({scrollHeight}) {
                   )}
                 </span>
               </button>
-              {/* Image fetch from google */}
-              <div className="flex-shrink-0 mr-1.5 md:mr-3">
-                  <img
-                  className="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10"
-                  src={photo}
-                  alt={name}
-                  />
-              </div>
-              {/* <div>
-                <p>{name}</p>
-              </div> */}
-
             </div>
           </div>
         </div>
