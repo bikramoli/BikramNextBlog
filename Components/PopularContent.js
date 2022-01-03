7267972d53fdee077ec16df81ba2a7d0868825d6
bikramoli <bikramoli75@gmail.com>
2022-01-03 12:10:56 +0545

    responsive screen- flex order

--- Components/PopularContent.js
+++ Components/PopularContent.js
@@ -8,7 +8,7 @@ function PopularContent({dat}) {
         setNewData(sortData)
     },[])
     return (      
-        <div>
+        <div className="popCoc">
             <h2 className=' m-2 mt-10 mb-4 font-bold text-indigo-500'>POPULAR CONTENT</h2>  
             {newData.map((d, i)=><Link key={i}  href={`/blogs/${d.data.Title.split(' ').join('-').toLowerCase()}`}><li 
             className="m-2 hover:text-red-700 underline list-none cursor-pointer">-{d.data.Title}</li></Link>)}                 
